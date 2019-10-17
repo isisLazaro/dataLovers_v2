9a86e1929579abf8b692947ac362101cdd016fcd
Isis Lázaro <isislazaro@gmail.com>
2019-10-17 09:28:53 -0500

    logo and navbar style

--- src/main.js
+++ src/main.js
@@ -30,11 +30,11 @@ const loadPage = async urlPage => {
       const page = urlPage.slice(48);
       paginationBox.innerHTML = /* html */ `
             <!-- pagination widget -->
-            <button id = "first-btn" class = "button is-info is-outlined ">
+            <button id = "first-btn" class = "button is-link is-outlined ">
                 <span class="icon"> <i class="fas fa-angle-double-left fa-lg"></i></span>
                 <span>first</span>
             </button>
-            <button id = "prev-btn" class = "button is-info">
+            <button id = "prev-btn" class = "button is-link">
                 <span class="icon"> <i class="fas fa-angle-left fa-lg"></i></span>
                 <span>prev</span>
             </button>
@@ -43,11 +43,11 @@ const loadPage = async urlPage => {
             <input class="input is-static has-text-left" type="text" value = ${totalPages} readonly>
             <!--<label for = "prev-btn"> ${page} </label>
             <label for = "next-btn"> / ${totalPages} </label>-->
-            <button id = "next-btn" class = "button is-info">
+            <button id = "next-btn" class = "button is-link">
                 <span>next</span>
                 <span class="icon"> <i class="fas fa-angle-right fa-lg"></i></span>
             </button>
-            <button id = "last-btn" class = "button is-info is-outlined">
+            <button id = "last-btn" class = "button is-link is-outlined">
                 <span>last</span>
                 <span class="icon"> <i class="fas fa-angle-double-right fa-lg"></i></span>
             </button>`;
